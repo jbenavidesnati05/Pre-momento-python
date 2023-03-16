0d5ba97771cc6d378b2720b2574f459280fb236f
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-03-16 18:03:47 -0500

    "punto 3 ok"

--- punto3.py
+++ punto3.py
@@ -20,6 +20,7 @@ while opcion != 0:
     print()
     if(opcion == 1):
         producto = {}
+        print("----------INGRESAR PRODUCTO-----------")
         producto['codigo'] = int(input("Digite el codigo del producto = "))
         producto['nombre'] = input("Digite el nombre del producto = ")
         producto['precio'] = int(input("Digite el precio del producto = "))
@@ -28,23 +29,41 @@ while opcion != 0:
         print("Producto registrado con exito")
         print()
     elif(opcion == 2):
-        print(f"listado de productos registrados => {productos}")
-        print()
+        if(len(productos) > 0):
+            print("----------LISTADO DE PRODUCTOS-----------")
+            print(f"listado de productos registrados => {productos}")
+        else:
+            print("la lista no tiene elementos!")
     elif(opcion == 3):
         print()
+        print("----------BUSCAR PRODUCTO-----------")
         buscado = int(input("Ingrese el codigo del producto a buscar = "))
         for producto in productos:
             if(buscado == producto['codigo']):
                 print(f"Producto encontrado => {producto}")
                 print()
-                break
             else:
                 print("Producto no encontrado =/ ")
     elif(opcion == 4):
+        print("----------EDITAR PRODUCTO-----------")
+        print()
+        buscado = int(input("Ingrese el codigo del producto a editar = "))
+        for producto in productos:
+            if(buscado == producto['codigo']):
+                print (f"producto a editar {buscado}")
+                producto['codigo'] = int(input("Ingrese el  NUEVO codigo del producto = "))
+                producto['nombre'] = (input("Ingrese el  NUEVO nombre del producto = "))
+                producto['precio'] = int(input("Ingrese el  NUEVO precio del producto = "))
+                print("Codigo editado con exito!")
+            else:
+                print("Producto no encontrado =/ ")
+    elif(opcion == 5):
+        print("----------ELIMINAR PRODUCTO-----------")
         print()
         buscado = int(input("Ingrese el codigo del producto a eliminar = "))
         for producto in productos:
             if(buscado == producto['codigo']):
+
                 print(f"Producto eliminar => {producto}")
                 productos.remove(producto)
                 print("Producto eliminado con exito! ")
